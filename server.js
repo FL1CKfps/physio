edab825c56cace6f667d86b86f95860f624e2864
FL1CKfps <117299453+FL1CKfps@users.noreply.github.com>
2025-03-13 15:21:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,27 +17,15 @@ let firebaseInitialized = false;
 try {
   console.log('Starting Firebase initialization...');
   
-  // Create a service account object directly
-  const serviceAccount = {
-    "type": "service_account",
-    "project_id": "assistant-df14d",
-    "private_key_id": "a78d82a1f9bb0906f81feab5f79b26a0ad8b2fc1",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC81u+kQcTXMBSu\nxu609NpoQEzUT49+y8dXolDfYWDLwdSUtNwDTfB/lxsqMT9+PfMI81ZeC9qzEPRU\nffNzDzyxvBYk90ZWOTJQpu+ZnL7mIYTgO4V5F4GnxGSX2RIr8W/dfzDoaT5wj9HF\nY9AleCDBntGaxMJlh+XQEGKeBhdOK6fNf2gNbaxaCcPUyQb/y1drVyT4mgaTuzAU\nnnEftjtyCsXlCC9DjhrOe47RILjgYkEWpJx8bqbaYUBVW1Nga2cu6bLxUyebOvMKB\nDwOilDP65TQBLsEwDLtQ1xZZota/+t2xdASVOH7J5cIp/G/Z+M2pE33yroqEjHC/\nwRrJXex/AgMBAAECggEAByIJSEeRs10mEtoBz0UGcPSo0kY1EeGJpJZFfsKTYO7v\n5DQWsWtCv9cJ2CSnFNIrOXXTl9bTynrD9+MQDJMRRFdp8LiEaCJd86zylPdWQKQY\nfE3TQ1kvdRJWiGPTK2TByHiS9Nxw1tif4FheVuP8PMQOf82Z2DLza9IKn5R0UegE\noHgLYzSayEQXEnyrMerNWeZ6PeiXGhxnFGY+1ZX+66SE8HoCFgvuUWJCYhjhFQAM\nA79D97L+zkHoUDAIOJXtSXW65CM+2Fd03Uug+cGovPmKtnhGDsIBFsYqYfMqh0UZ\ncijiLKZLGrEy+MPysdQWAvxaCx1JeK8OvwuhDNKHsQKBgQDY5EL+lWe0PvlLDFan\niyD4o0PYUygXklTXi7TAuX0Q4nzeCU8qrLvlsLkibh3Su6O8s5w7UTEXwAkikYkS\nnoHmUeufarHUY3Z7rDJVHnPRLzARU7B2Vy3B09mRQLCxB+TXiFxZunXJ2NbVaet5J\nvvsSDBAO8i9Ic+5jN3fTojGYsQKBgQDe48n/9hXK7Wulwob8K4AzleLIppmXcfcA\nZTFx0HK+r09hxqAnAoPTOTyJO0wZQ14U2N44Bln1OHs3bWkZOxJOlF44YiavEF4x\n96f75EKGcY0WTcpujonHl2Jd7MXmyjh5K/1F+WE7GQtDPAbijEBmPPTZavqR2fXb\nrSqAp+okLwKBgQCU5G5RUsN0a1679GZwcgRaa8ohcUSumdgAeHnHbA69Tp4l1wIv\n65C5543QYwO6LPIEktCHr/8hrMUYpAwTa3LLSWxtI8LnJQz5kxPPq6HRfubiXY0Z\nziUPRi4rE4f2alxhLzRJD3EKUWiECqEawy6cfwdvGzb2aA+YsPVjpY6ioQKBgCso\njGqlqd6bTmbQXfbiLXbPpRQZPlBW3u8SqRpStIU7yPEz1rr2/7Oct54S/088/jCn\nNZA/mpejEijPZLRf/dF9ZSkhyD/JuZOPUtWxgzgpMJqLAZGaxA8DvyZUvJVNvtWn\nsOklurAIz2d7bhHb+nRRhU+8KTyoqbSZuUskOHA9AoGBAMMV0tFJYkYntWShAlKs\nYEV073QTnjpSDqxVe8KRWteIxqXu8b0ZCExF0q8OJVWY6aEon78UNflqWa/dfc5j\nzsfEVKhPLOiIBPUxUWMyg+nduAVRGR5LaU2q8CkjjIzpKvg6ppNcMpsYYNtqncB8\n5s9GvQX6UXmHqPDrdy80e8+9\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-aa2fx@assistant-df14d.iam.gserviceaccount.com",
-    "client_id": "110415566292636180626",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-aa2fx%40assistant-df14d.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-  };
-
-  console.log('Using complete service account JSON format');
-
+  // Import service account file directly
+  const serviceAccountPath = new URL('./service-account-key.json', import.meta.url);
+  
+  console.log('Loading service account from:', serviceAccountPath.pathname);
+  
   // Check if Firebase is already initialized
   if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
+      credential: admin.credential.cert(serviceAccountPath.pathname)
     });
     
     // Test the connection
